8b16e4d770a03feec9711d1de784bbdead908f00
Zlata <zshimova@bk.ru>
2024-01-19 16:15:53 +0300

    second commit

--- index.js
+++ index.js
@@ -40,63 +40,63 @@
 // ********************************
 // Упражнение №3
 
-const wsUri = 'wss://echo-ws-service.herokuapp.com';
-const inputData = document.querySelector('input');
-const sendBtn = document.querySelector('.send-btn')
-const sendGeo = document.querySelector('.send-geo');
-const wrapperChat = document.querySelector('.chat-wrapper');
-const userMessages = document.querySelector('.user-messages');
+// const wsUri = 'wss://echo-ws-service.herokuapp.com';
+// const inputData = document.querySelector('input');
+// const sendBtn = document.querySelector('.send-btn')
+// const sendGeo = document.querySelector('.send-geo');
+// const wrapperChat = document.querySelector('.chat-wrapper');
+// const userMessages = document.querySelector('.user-messages');
 
 
-//Вывод сообщения
-function writeToScreen(message, position = 'flex-end') {
-    let element = `
-        <p class='messages' style='align-self: ${position}'>
-            ${message}
-        </p>
-    `;
-    userMessages.innerHTML += element;
-    wrapperChat.scrollTop = wrapperChat.scrollHeight;
-}
+// //Вывод сообщения
+// function writeToScreen(message, position = 'flex-end') {
+//     let element = `
+//         <p class='messages' style='align-self: ${position}'>
+//             ${message}
+//         </p>
+//     `;
+//     userMessages.innerHTML += element;
+//     wrapperChat.scrollTop = wrapperChat.scrollHeight;
+// }
 
-// Объект соединения
-let websocket = new WebSocket(wsUri);
-websocket.onopen = function(evt) {
-    console.log("CONNECTED");
-};
-websocket.onmessage = function(evt) {
-    writeToScreen(`ответ сервера: ${evt.data}`, 'flex-start');
-};
-websocket.onerror = function(evt) {
-    writeToScreen(`server: ${evt.data}`, 'flex-start');
-};
+// // Объект соединения
+// let websocket = new WebSocket(wsUri);
+// websocket.onopen = function(evt) {
+//     console.log("CONNECTED");
+// };
+// websocket.onmessage = function(evt) {
+//     writeToScreen(`ответ сервера: ${evt.data}`, 'flex-start');
+// };
+// websocket.onerror = function(evt) {
+//     writeToScreen(`server: ${evt.data}`, 'flex-start');
+// };
 
-// Отправить сообщение
-sendBtn.onclick = () => {
-    let message = inputData.value;
-    websocket.send(message);
-    writeToScreen(`Вы: ${message}`);
-    inputData.value = ''
-}
+// // Отправить сообщение
+// sendBtn.onclick = () => {
+//     let message = inputData.value;
+//     websocket.send(message);
+//     writeToScreen(`Вы: ${message}`);
+//     inputData.value = ''
+// }
 
-// Если получение геолокации запрещено
-const error = () => {
-    let textErr0r = 'Невозможно получить ваше местоположение';
-    writeToScreen(textErr0r);
-};
+// // Если получение геолокации запрещено
+// const error = () => {
+//     let textErr0r = 'Невозможно получить ваше местоположение';
+//     writeToScreen(textErr0r);
+// };
 
-// Успешное получениие геолокации
-const success = (position) => {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    let geoLink = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    writeToScreen(`<a  href='${geoLink}' target='_blank'>Ваша гео-локация</a>`);
-};
+// // Успешное получениие геолокации
+// const success = (position) => {
+//     let latitude = position.coords.latitude;
+//     let longitude = position.coords.longitude;
+//     let geoLink = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+//     writeToScreen(`<a  href='${geoLink}' target='_blank'>Ваша гео-локация</a>`);
+// };
 
-sendGeo.addEventListener('click', () => {
-    if (!navigator.geolocation) {
-        console.log('Geolocation не поддерживается вашим браузером');
-    } else {
-        navigator.geolocation.getCurrentPosition(success, error);
-    }
-});
+// sendGeo.addEventListener('click', () => {
+//     if (!navigator.geolocation) {
+//         console.log('Geolocation не поддерживается вашим браузером');
+//     } else {
+//         navigator.geolocation.getCurrentPosition(success, error);
+//     }
+// });
